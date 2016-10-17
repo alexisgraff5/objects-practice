142d99b412fd644e7c4c94480b1cf675009b8705
alexisgraff5 <lex.graff@gmail.com>
2016-10-17 11:49:11 -0600

    user1 part 1 problem complete

--- practice.js
+++ practice.js
@@ -180,6 +180,11 @@ var user1 = {
 that each value is truthy. If it's not truthy, remove it from the object. */
 
   //Code Here
+  for (var key in user1) {
+    if (!user1[key]) {
+      delete user1[key];
+    }
+  }
 
 //Once you get your truthy Object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
 
