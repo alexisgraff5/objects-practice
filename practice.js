f939fba666d5df749500771d96a46d40a38b1848
alexisgraff5 <lex.graff@gmail.com>
2016-10-17 11:34:47 -0600

    states problem complete

--- practice.js
+++ practice.js
@@ -143,11 +143,23 @@ Instead, console.log your whole backPack object and then check out the console. 
 //Create an object called states that has 5 US states as properties with the values being their population (doesn't have to be accurate).
 
   //Code Here
+  var states = {
+    Utah: 1000000,
+    Idaho: 750000,
+    Texas: 2500000,
+    Montana: 350000,
+    Wyoming: 400000
+  };
 
 //Now, loop through your states object and if the states population is greater than 30K, alert that state.
 
   //Code Here
-
+  for (var key in states) {
+    if (states[key] > 30000) {
+      //alert([key]);
+      console.log([key]);
+    }
+  }
 
 
 
